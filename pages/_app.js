0e4dd1b9c6e3c76fb2585a68db1f50baacde8326
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-11-05 23:47:18 +0600

    Initial commit from the new site project

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../global.styles.css";
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
 import React from "react";
-import Hello from "../src/components/hello/hello";
+import Head from "next/head";
 
-const Home: React.FC = () => {
-  return <Hello name="Prottay" />;
+const AnswersTable: React.FC = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Water Stadia</title>
+      </Head>
+      <h1 className="text-xl text-center">Some Applications I've Worked On</h1>
+      <div className={styles.container}>
+        <table className={styles.answerTableD}>
+          <thead>
+            <tr>
+              <th>Application</th>
+              <th>Answer</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <p className={styles.footerName}>Prottoi</p>
+    </>
+  );
 };
 
-export default Home;
+export default AnswersTable;
