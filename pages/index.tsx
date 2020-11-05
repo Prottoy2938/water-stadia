cd30a6c7bcfac1bd477193af078bee53edf9fd49
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-11-05 18:25:56 +0000

    Added table with app names

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,34 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
-import {Heading, Box, Text, useColorMode} from "@chakra-ui/core"
+import { Heading, Box, Text, useColorMode, Link } from "@chakra-ui/core";
 
 const AnswersTable: React.FC = () => {
+  // const { colorMode, setColorMode } = useColorMode();
 
-  const { colorMode, setColorMode } = useColorMode();
-
-  useEffect(() => {
-    if (!colorMode) {
-      if (window.localStorage) {
-        const savedTheme = window.localStorage.getItem("chakra-ui-color-mode");
-        if (savedTheme == "light" || savedTheme == "dark") {
-          setColorMode(savedTheme);
-        } else {
-          setColorMode("dark");
-        }
-      } else {
-        setColorMode("dark");
-      }
-    }
-  }, [colorMode, setColorMode]);
-
+  // useEffect(() => {
+  //   if (!colorMode) {
+  //     if (window.localStorage) {
+  //       const savedTheme = window.localStorage.getItem("chakra-ui-color-mode");
+  //       if (savedTheme == "light" || savedTheme == "dark") {
+  //         setColorMode(savedTheme);
+  //       } else {
+  //         setColorMode("dark");
+  //       }
+  //     } else {
+  //       setColorMode("dark");
+  //     }
+  //   }
+  // }, [colorMode, setColorMode]);
 
   return (
     <>
       <Head>
         <title>Water Stadia</title>
       </Head>
-      <Heading mt={3} mb={5} as="h1" size="2xl" textAlign="center">Applications I've Worked On</Heading>
-      <Box w="500px" m="0 auto">
+      <Heading mt={3} mb={5} as="h1" size="2xl" textAlign="center">
+        Applications I've Worked On
+      </Heading>
+      <Box maxWidth="90%" m="0 auto">
         <table className="answerTableD">
           <thead>
             <tr>
@@ -39,14 +39,127 @@ const AnswersTable: React.FC = () => {
           </thead>
           <tbody>
             <tr>
-              <td>Ingredient</td>
-              <td>http://ingredient.now.sh/</td>
-              <td>Not Open Source</td>
+              <td>
+                <Link
+                  href="https://ingredient.now.sh/"
+                  color="cyan"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  Ingredient
+                </Link>
+              </td>
+              <td>
+                A web application that helps you find food dishes and provides
+                detailed info about them.
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <Link
+                  href="https://backdrop.vercel.app/"
+                  color="cyan"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  Backdrop{" "}
+                </Link>
+              </td>
+
+              <td>
+                A web application to change/modify webcam stream and record
+              </td>
+              <td>
+                <Link
+                  href="https://github.com/Prottoy2938/backdrop"
+                  color="cyan"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  GitHub
+                </Link>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <Link
+                  href="https://cluster-11.github.io/open-mind/"
+                  color="cyan"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  Open Mind
+                </Link>
+              </td>
+
+              <td>
+                a web application that you can use to train a machine learning
+                model and make it recognize certain images
+              </td>
+              <td>
+                <Link
+                  href="https://github.com/cluster-11/open-mind"
+                  color="cyan"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  GitHub
+                </Link>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <Link
+                  href="https://cipher-delta.edrini.xyz/"
+                  color="red"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  Cipher Delta
+                </Link>
+              </td>
+
+              <td>
+                A web application that helps encode and decode plain content
+              </td>
+              <td>
+                <Link
+                  href="https://github.com/Prottoy2938/cipher-delta"
+                  color="green"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  GitHub
+                </Link>
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <Link
+                  href="https://change-svg-color.edrini.xyz/"
+                  color="red"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  Change SVG Color
+                </Link>
+              </td>
+
+              <td>
+                A web application that helps change SVG Color Using CSS Filter
+              </td>
+              <td>
+                <Link
+                  href="https://github.com/Prottoy2938/change-svg-color"
+                  color="green"
+                  _hover={{ cursor: "pointer", textDecoration: "underline" }}
+                >
+                  GitHub
+                </Link>
+              </td>
             </tr>
           </tbody>
         </table>
       </Box>
-      <Text pos="fixed" right={5} bottom={5}>- Prottoi</Text>
+      <Text pos="fixed" right={5} bottom={5}>
+        - Prottoi
+      </Text>
     </>
   );
 };
